886f6a6905ae7a5383d95d3059ffa4583621b0aa
JedPogi <mangjuan0812@gmail.com>
2025-10-15 19:43:13 +0200

    uploaded page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage-component/homepage-component';
+import { SkillsComponent } from './pages/skills-component/skills-component';
 
 export const routes: Routes = [
-      {
-    path: '',
-    loadComponent: () => import('./pages/homepage-component/homepage-component').then(m=>m.HomepageComponent)
-  },
+    {
+        path: '',
+        loadComponent: () => import('./pages/homepage-component/homepage-component').then(m => m.HomepageComponent)
+    },
+    {
+        path: 'skills', 
+        loadComponent: () => import('./pages/skills-component/skills-component').then(m => m.SkillsComponent)
+    },
 ];
---
+++ src/app/pages/skills-component/skills-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar-component/navbar-component";
+@Component({
+  selector: 'app-skills-component',
+  imports: [NavbarComponent],
+  templateUrl: './skills-component.html',
+  styleUrl: './skills-component.scss'
+})
+export class SkillsComponent {
+
+}
