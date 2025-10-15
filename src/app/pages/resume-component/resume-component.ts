ebb2786263255212168636ab60ca5ee8b12b51f8
JedPogi <mangjuan0812@gmail.com>
2025-10-15 21:07:26 +0200

    updated files

--- src/app/pages/resume-component/resume-component.ts
+++ src/app/pages/resume-component/resume-component.ts
@@ -9,4 +9,16 @@ import { NavbarComponent } from "../../components/navbar-component/navbar-compon
 })
 export class ResumeComponent {
 
+
+
+  downloadMyFile(){
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'src/assets');
+    link.setAttribute('download', `updated_curriculum_2025.pdf`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
 }
