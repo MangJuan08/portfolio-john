759e42828432dfca8e582598b33ca801af675396
JedPogi <mangjuan0812@gmail.com>
2025-10-15 19:27:31 +0200

    push new files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage-component/homepage-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    path: '',
+    loadComponent: () => import('./pages/homepage-component/homepage-component').then(m=>m.HomepageComponent)
+  },
+];
