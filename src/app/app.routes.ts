de3e6726ef88c15ed0891295d7a70673a853970e
JedPogi <mangjuan0812@gmail.com>
2025-10-15 22:03:13 +0200

    remove skills page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,6 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/homepage-component/homepage-component').then(m => m.HomepageComponent)
     },
     {
-        path: 'skills', 
-        loadComponent: () => import('./pages/skills-component/skills-component').then(m => m.SkillsComponent)
-    },
-    {
         path: 'my-projects', 
         loadComponent: () => import('./pages/my-projects-component/my-projects-component').then(m => m.MyProjectsComponent)
     },
