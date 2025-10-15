b2697e100606d7b349b6088d231ec0be1fe2f6d9
JedPogi <mangjuan0812@gmail.com>
2025-10-15 19:34:12 +0200

    added card

--- src/app/pages/homepage-component/homepage-component.ts
+++ src/app/pages/homepage-component/homepage-component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar-component/navbar-component";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-homepage-component',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,MatCardModule, MatButtonModule],
   templateUrl: './homepage-component.html',
   styleUrl: './homepage-component.scss'
 })
