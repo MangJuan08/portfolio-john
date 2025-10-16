065434bd351c9bf99a42504cef02842c80fc45fb
JedPogi <mangjuan0812@gmail.com>
2025-10-16 13:29:09 +0200

    edited routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/my-projects-component/my-projects-component').then(m => m.MyProjectsComponent)
     },
     {
-        path: 'emplotment-history', 
+        path: 'employment-history', 
         loadComponent: () => import('./pages/employment-history-component/employment-history-component').then(m => m.EmploymentHistoryComponent)
     },
     {
